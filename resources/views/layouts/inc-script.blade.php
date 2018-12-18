3ac7f6651c7fa4e0b0af2d7a0857f29fb5ed98d6
kimkundad <kim.kundad@gmail.com>
2018-12-18 16:32:08 +0700

    golf

--- resources/views/layouts/inc-script.blade.php
+++ resources/views/layouts/inc-script.blade.php
@@ -9,22 +9,21 @@
 
         <!-- Load Facebook SDK for JavaScript -->
         <!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="943133742544793"
-  logged_in_greeting="Piccurated online printing"
-  logged_out_greeting="Piccurated online printing">
-</div>
+        <!-- Your customer chat code -->
+        <div class="fb-customerchat"
+          attribution=setup_tool
+          page_id="943133742544793">
+        </div>
 
         <script>
                 $(document).ready(function(){
