cc88c886a1d0e1aa7ed58e3dada3b66347024cb6
kimkundad <kim.kundad@gmail.com>
2018-12-18 14:41:49 +0700

    golf

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -172,6 +172,9 @@
                                   <a href="javascript:$('#my_form').submit();" id="subtip_to" style="display:none">Add to cart</a>
                                   <a  id="subtip_weit" data-toggle="modal" data-target="#exampleModalCenter" style="display:none">Add to cart</a>
 
+                                  <div class="fb-share-button" data-href="https://piccurated.com/product/{{$objs->id_p}}" data-layout="box_count" data-size="small" data-mobile-iframe="true">
+                                    <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fpiccurated.com%2Fproduct%2F{{$objs->id_p}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">แชร์</a>
+                                  </div>
 
                             </div>
                             <hr />
