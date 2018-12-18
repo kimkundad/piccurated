2cca5b70b3ab66dfb5c8d7bdf30aa84325602122
kimkundad <kim.kundad@gmail.com>
2018-12-18 17:34:34 +0700

    golf

--- resources/views/layouts/inc-script.blade.php
+++ resources/views/layouts/inc-script.blade.php
@@ -16,7 +16,14 @@
 
         <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>
+<script>window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '943133742544793', // app id สามารถหาได้จากการสร้าง fb apps ดูลิงค์ด้านล่าง
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
 
   (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
