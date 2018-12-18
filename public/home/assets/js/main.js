d98ecf094c578f95af26d81be9c5e7f9266f3c60
kimkundad <kim.kundad@gmail.com>
2018-12-18 16:14:34 +0700

    golf

--- public/home/assets/js/main.js
+++ public/home/assets/js/main.js
@@ -104,7 +104,6 @@
         autoplay:true,
         autoplayTimeout:2500,
         mouseDrag: false,
-        nav:true,
         dots: false,
         navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
         responsive:{
