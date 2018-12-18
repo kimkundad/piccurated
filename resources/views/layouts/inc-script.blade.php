83020d8b73896b5491711f257d8d9beca5e83e0d
kimkundad <kim.kundad@gmail.com>
2018-12-18 16:36:15 +0700

    golf

--- resources/views/layouts/inc-script.blade.php
+++ resources/views/layouts/inc-script.blade.php
@@ -1,11 +1,6 @@
 <!-- all js here -->
         <script src="{{url('home/assets/js/vendor/jquery-3.2.1.min.js')}}"></script>
-        <script src="{{url('home/assets/js/popper.js')}}"></script>
-        <script src="{{url('home/assets/js/bootstrap.min.js')}}"></script>
-        <script src="{{url('home/assets/js/owl.carousel.min.js')}}"></script>
-        <script src="{{url('home/assets/js/jquery.meanmenu.js')}}"></script>
-        <script src="{{url('home/assets/js/plugins.js')}}"></script>
-        <script src="{{url('home/assets/js/main.js')}}?v1.8"></script>
+
 
         <!-- Load Facebook SDK for JavaScript -->
         <!-- Load Facebook SDK for JavaScript -->
@@ -24,6 +19,17 @@
           attribution=setup_tool
           page_id="943133742544793">
         </div>
+
+
+        
+        <script src="{{url('home/assets/js/popper.js')}}"></script>
+        <script src="{{url('home/assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{url('home/assets/js/owl.carousel.min.js')}}"></script>
+        <script src="{{url('home/assets/js/jquery.meanmenu.js')}}"></script>
+        <script src="{{url('home/assets/js/plugins.js')}}"></script>
+        <script src="{{url('home/assets/js/main.js')}}?v1.8"></script>
+
+
 
         <script>
                 $(document).ready(function(){
