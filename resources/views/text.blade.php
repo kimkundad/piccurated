b9c250791733ba92c0b7298d83bfcaad919f7455
kimkundad <kim.kundad@gmail.com>
2018-12-18 17:03:56 +0700

    golf

--- resources/views/text.blade.php
+++ resources/views/text.blade.php
@@ -1,23 +1,32 @@
 
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <h2>Say Hi</h2>
+        <div class="fb-customerchat"
+          attribution=setup_tool
+          page_id="943133742544793"
+          theme_color="#6699cc"
+          logged_in_greeting="piccurated  natural printing."
+          logged_out_greeting="piccurated  natural printing.">
+        </div>
 
 
-<h2>Say Hi</h2>
+    </body>
 
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
-
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="943133742544793"
-  theme_color="#6699cc"
-  logged_in_greeting="piccurated  natural printing."
-  logged_out_greeting="piccurated  natural printing.">
-</div>
+    <!-- Your customer chat code -->
+</html>
