0330848b8505d874d0bc292c516925028bdd2cf8
kimkundad <kim.kundad@gmail.com>
2018-12-18 17:17:11 +0700

    golf

--- resources/views/admin/layouts/inc-scripts.blade.php
+++ resources/views/admin/layouts/inc-scripts.blade.php
@@ -38,3 +38,21 @@
 		 <script src="{{asset('/assets/javascripts/tables/examples.datatables.default.js')}}"></script>
 		 <script src="{{asset('/assets/javascripts/pages/examples.mediagallery.js')}}"></script>
 		 <script src="{{asset('/assets/javascripts/ui-elements/examples.loading.overlay.js')}}"></script>
+
+
+		 <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+attribution=setup_tool
+page_id="943133742544793"
+theme_color="#6699cc">
+</div>
