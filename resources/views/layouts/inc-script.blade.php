1341899c36afc0a3ab6746318633d36765a84d25
kimkundad <kim.kundad@gmail.com>
2018-12-18 17:15:00 +0700

    golf

--- resources/views/layouts/inc-script.blade.php
+++ resources/views/layouts/inc-script.blade.php
@@ -14,6 +14,24 @@
         <script src="{{url('home/assets/js/main.js')}}?v1.8"></script>
 
 
+        <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="943133742544793"
+  theme_color="#6699cc">
+</div>
+
+
 
         <script>
                 $(document).ready(function(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -673,21 +673,5 @@ piccurated is online natural printing, art gallery. Connecting between art, phot
 
 <!-- Load Facebook SDK for JavaScript -->
 
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="943133742544793"
-  theme_color="#6699cc"
-  logged_in_greeting="piccurated  natural printing."
-  logged_out_greeting="piccurated  natural printing.">
-</div>
 
 @stop('scripts')
